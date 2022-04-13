254a602608ce4e14a5c06e86875c60338a6e859f
Eva Miriam <102222001+evamiriaam@users.noreply.github.com>
2022-04-13 19:35:21 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,12 +5,12 @@ let pi;
 let piShow = '';
 
 let digitsDiv;
-let digits = "3.";
+let digits = '3.';
 let piCounter = 0;
 
 let gameOver = false;
 
-function preload(){
+function preload() {
   pi = loadStrings('one-million.txt');
 }
 
@@ -20,7 +20,7 @@ function setup() {
   plate = new Plate(width / 2, 50);
   digitsDiv = createDiv(digits);
   digitsDiv.style('font-size', '64pt');
-  piShow = pi.substring(0, 24);
+  piShow = pi.substring(0, 2);
 }
 
 function draw() {
@@ -28,44 +28,40 @@ function draw() {
     background(255, 0, 0);
     fill(255);
     textAlign(CENTER, CENTER);
-    text("Game Over. \nGo and Enjoy Some Pie! ✨", width/2, height/2);
+    text('Game Over.\nGo and Enjoy Some Pie! ✨', width / 2, height / 2);
     return;
   }
 
   background(0);
 
   fill(255);
-  textSize(32);
-  text(piShow, width-64, 32);
+  textSize(48);
+  text(piShow, width - 64, 50);
   fill(0, 255, 0);
-  textSize(32);
-  text(piShow.charAt, width-64, 50);
+  text(piShow.charAt(0), width - 64, 50);
 
   if (random(1) < 0.1) {
     pies.push(new Pie(random(width), random(-100, -20)));
   }
+
   for (let pie of pies) {
-  pies.show();
-  pies.update();
-  if (plate.catches(pies[i])) {
-    console.log("🤗");
+    pies.show();
+    pies.update();
   }
-}
 
-  for (let i = pies.length-1; i >= 0; i--) {
+  for (let i = pies.length - 1; i >= 0; i--) {
     if (plate.catches(pies[i])) {
-    // catch the pie
-    // Check what digit was caught and deal with score
-    let digit = pies[i].digit;
-    digits += digit;
+      // catch the pie
+      // check what digit was caught and deal with score!
+      let digit = pies[i].digit;
 
-    let correctDigit = pi.charAt(piCounter)
-    if (correctDigit == digit) {
-      console.log("✨");
-      digits += digit;
-      piCounter++;
-        piShow = pi.substring(piCounter, piCounter + 24);
-    } else {
+      let correctDigit = pi.charAt(piCounter);
+      if (correctDigit == digit) {
+         console.log("✨");
+         digits += digit;
+         piCounter++;
+         piShow = pi.substring(piCounter, piCounter + 2);
+      } else {
       gameOver = true;
       //console.log("❤️");
     }
@@ -77,11 +73,7 @@ function draw() {
       pies.splice(i, 1);
     }
   }
-  console.log(pies.length);
 
   plate.x = mouseX;
   plate.show();
-  
-  rectMode(CENTER);
-  rect(mouseX,height-10,100,10);
 }
