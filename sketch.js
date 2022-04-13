7d74bb4bcce625d9b43faf24bf8d657a84ed0e74
Eva Miriam <102222001+evamiriaam@users.noreply.github.com>
2022-04-13 18:17:22 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,25 @@
-let pie;
+let pies = [];
 let plate;
-
 
 function setup() {
   createCanvas(400, 400);
-  pie = new Pie(100, 100);
-  plate = new Plate(width/2, 100);
+  for (let i = 0; i < 314; i++) {
+   pies.push(new Pie(100, 100));
+  }
+  plate = new Plate(width / 2, 100);
 }
 
 function draw() {
   background(0);
   
-  pie.show();
-  pie.update();
+  for (let i = 0; i < pies.length; i++) {
+  pies[i].show();
+  pies[i].update();
   
-  if (plate.catches(pie)) {
-    console.log("🤗")
+  if (plate.catches(pies[i])) {
+    console.log("🤗");
   }
-  
+}
   plate.x = mouseX;
   plate.show();
   
