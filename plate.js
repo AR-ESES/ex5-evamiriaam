a927d1b626a18e3266ffadbd414ae9c60b59601f
Eva Miriam <102222001+evamiriaam@users.noreply.github.com>
2022-04-13 17:56:58 +0100

    Add files via upload

---
+++ plate.js
@@ -0,0 +1,22 @@
+class Plate {
+  constructor(x, w) {
+    this.x = x;
+    this.w = w;
+    this.h = 10;
+    this.y = height - this.h;
+  }
+  
+  catches(pie) {
+    if (pie.y >= this.y) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  show(){
+    rectMode(CENTER);
+    rect(this.x, this.y, this.w, this.h);
+  }
+  
+}
